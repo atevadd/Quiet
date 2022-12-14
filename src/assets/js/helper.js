cd6b2abf6df71c02ce8a69cfd29dea1fa771cd3e
Adeniyi Tofunmi <adeniyitofunmi@gmail.com>
2022-12-14 09:41:41 +0100

    chore: cookie functions to remember to user

--- src/assets/js/helper.js
+++ src/assets/js/helper.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// Verify user before entering route
 export const verifyUser = async (username) => {
   try {
     const response = await axios.get(`/${username}`);
@@ -13,3 +14,26 @@ export const verifyUser = async (username) => {
     console.log(error);
   }
 };
+
+// Set cookies
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Get Cookies
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
