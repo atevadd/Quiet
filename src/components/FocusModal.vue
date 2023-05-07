06ebde1aa234f14daf64fa4531301f1614f49b16
Adeniyi Tofunmi <adeniyitofunmi@gmail.com>
2023-05-07 22:42:30 +0100

    feat: share message button

--- src/components/FocusModal.vue
+++ src/components/FocusModal.vue
@@ -14,7 +14,7 @@
           {{ store.focus.message }}
         </p>
       </div>
-      <button id="download-message" @click="downloadMessage">
+      <button id="download-message" @click="shareMessage">
         <i class="uil uil-share-alt"></i>Share Message
       </button>
     </div>
@@ -27,7 +27,7 @@ import { useStore } from "../stores/store";
 
 const store = useStore();
 
-const downloadMessage = () => {
+const shareMessage = () => {
   // Step 1: Include html2canvas library in your HTML file
 
   // Step 2: Select the element to capture
@@ -44,122 +44,41 @@ const downloadMessage = () => {
         navigator
           .share({
             files: [new File([blob], "screenshot.png", { type: "image/png" })],
+            title: "Share anonymous message",
+            text: "You are sharing one of the anonymous messages you received",
           })
           .then(() => {
             console.log("Successfully shared!");
           })
           .catch((error) => {
             console.error("Error sharing:", error);
+
+            // Get the image data as a base64-encoded string
+            const imageData = canvas.toDataURL("image/png");
+
+            // Step 4: Create a download link
+            const downloadLink = document.createElement("a");
+            downloadLink.setAttribute("href", imageData);
+            downloadLink.setAttribute("download", "anon.png");
+
+            // Step 5: Trigger the download
+            downloadLink.click();
           });
       } else {
         console.error("Web Share API is not supported in this browser.");
+        // Get the image data as a base64-encoded string
+        const imageData = canvas.toDataURL("image/png");
+
+        // Step 4: Create a download link
+        const downloadLink = document.createElement("a");
+        downloadLink.setAttribute("href", imageData);
+        downloadLink.setAttribute("download", "anon.png");
+
+        // Step 5: Trigger the download
+        downloadLink.click();
       }
     }, "image/png");
   });
-
-  // elementToCapture.style.fontFeatureSettings = '"liga" 0';
-  // // Step 3: Capture the element as an image
-  // html2canvas(elementToCapture, {
-  //   backgroundColor: null,
-  // }).then(async (canvas) => {
-  //   // Get the image data as a base64-encoded string
-  //   const imageData = canvas.toDataURL("image/png");
-
-  //   // Step 4: Share using Web Share API
-  //   if (navigator.share) {
-  //     await navigator
-  //       .share({
-  //         title: "Anonymous Message",
-  //         text: "Check out this anonymous message!",
-  //         url: imageData,
-  //       })
-  //       .then(() => console.log("Screenshot shared successfully."))
-  //       .catch((error) => {
-  //         console.error("Error occured.");
-  //         // Get the image data as a base64-encoded string
-  //         const imageData = canvas.toDataURL("image/png");
-
-  //         // Step 4: Create a download link
-  //         const downloadLink = document.createElement("a");
-  //         downloadLink.setAttribute("href", imageData);
-  //         downloadLink.setAttribute("download", "anon.png");
-
-  //         // Step 5: Trigger the download
-  //         downloadLink.click();
-  //       });
-  //   } else {
-  //     console.error("Web Share API not supported.");
-
-  //     // Get the image data as a base64-encoded string
-  //     const imageData = canvas.toDataURL("image/png");
-
-  //     // Step 4: Create a download link
-  //     const downloadLink = document.createElement("a");
-  //     downloadLink.setAttribute("href", imageData);
-  //     downloadLink.setAttribute("download", "anon.png");
-
-  //     // Step 5: Trigger the download
-  //     downloadLink.click();
-  //   }
-  // });
-
-  // // Step 3: Capture the element as an image
-  // html2canvas(elementToCapture).then((canvas) => {
-  //   // Get the image data as a base64-encoded string
-  //   const imageData = canvas.toDataURL("image/png");
-
-  //   // Step 4: Create a download link
-  //   const downloadLink = document.createElement("a");
-  //   downloadLink.setAttribute("href", imageData);
-  //   downloadLink.setAttribute("download", "anon.png");
-
-  //   // Step 5: Trigger the download
-  //   downloadLink.click();
-  // });
-
-  // Step 1: Select the element to capture
-  // const elementToCapture = document.getElementById("focus-modal");
-
-  // // // Step 2: Create a temporary canvas
-  // // const canvas = document.createElement("canvas");
-
-  // // // Step 3: Get the dimensions of the element
-  // // const elementWidth = elementToCapture.offsetWidth;
-  // // const elementHeight = elementToCapture.offsetHeight;
-
-  // // // Step 4: Set the canvas dimensions
-  // // canvas.width = elementWidth;
-  // // canvas.height = elementHeight;
-
-  // // // Step 5: Get the canvas context
-  // // const context = canvas.getContext("2d");
-
-  // // // Step 6: Convert the element to an HTMLImageElement
-  // // const image = new Image();
-
-  // // // Step 7: Serialize the SVG content of the element
-  // // const serializer = new XMLSerializer();
-  // // const svgString = serializer.serializeToString(elementToCapture);
-  // // const encodedSvg = encodeURIComponent(svgString);
-  // // console.log(svgString);
-  // // image.src = `data:image/svg+xml;charset=utf-8,${encodedSvg}`;
-
-  // // // Step 8: Wait for the image to load
-  // // image.onload = function () {
-  // //   // Step 9: Draw the element on the canvas
-  // //   context.drawImage(image, 0, 0, elementWidth, elementHeight);
-
-  // //   // Step 10: Capture the image data
-  // //   canvas.toBlob(function (blob) {
-  // //     // Create a download link for the captured image
-  // //     const downloadLink = document.createElement("a");
-  // //     downloadLink.href = URL.createObjectURL(blob);
-  // //     downloadLink.download = "screenshot.png";
-
-  // //     // Trigger the download
-  // //     downloadLink.click();
-  // //   });
-  // // };
 };
 </script>
 
